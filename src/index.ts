9481c331a72fd9949d76e9f27b4660a17f4a17cf
Joe Flateau <joe@joeflateau.net>
2019-10-25 20:37:50 -0400

    init

---
+++ src/index.ts
@@ -0,0 +1,20 @@
+import chalk from 'chalk';
+
+console.log(
+  '\n' +
+    `
+  ${chalk.rgb(64, 164, 255)(`Joe Flateau`)}
+  ${chalk.rgb(200, 200, 200)(`joe@joeflateau.net`)}
+  ${chalk.rgb(112, 112, 112)('-'.repeat(32))}
+
+  https://joeflateau.net
+  https://twitter.com/joeflateau
+  https://github.com/joeflateau
+
+`
+      .trim()
+      .split('\n')
+      .map(line => line.trim())
+      .join('\n') +
+    '\n'
+);
