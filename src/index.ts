7f10a60eb19ae8149a76cd00f3443aae955f0545
Joe Flateau <joe@joeflateau.net>
2019-10-26 10:04:06 -0400

    cool banner and formatting

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,25 @@
 #!/usr/bin/env node
-import chalk from 'chalk';
+import chalk from "chalk";
+import { textSync } from "figlet";
 
 const blue = chalk.rgb(64, 164, 255);
 const gray = (v: number) => chalk.rgb(v, v, v);
 
+const banner = (label: string, width = 64) => {
+  return `\n-- ${label} ${"-".repeat(width - 6)}\n`;
+};
+
+const line = (text: string) => `   ${text}`;
+
 const message = [
-  blue.bold`Joe Flateau`,
-  gray(120)('-'.repeat(32)),
-  gray(190)`joe@joeflateau.net`,
-  gray(190)`https://joeflateau.net`,
-  gray(120)('-'.repeat(32)),
-  gray(190)`https://twitter.com/joeflateau`,
-  gray(190)`https://github.com/joeflateau`,
-  gray(190)`https://linkedin.com/in/joeflateau`,
-  // gray(120)(''),
-  // gray(190)(
-  //   `${chalk.bold(
-  //     'Make one of these:'
-  //   )} ${'https://joeflateau.net/posts/npx-yourself'}`
-  // ),
+  blue(textSync(`Joe Flateau`, "ANSI Shadow")),
+  gray(120)(banner("Contact")),
+  gray(190)(line(`joe@joeflateau.net`)),
+  gray(190)(line(`https://joeflateau.net`)),
+  gray(120)(banner("Social")),
+  gray(190)(line(`https://twitter.com/joeflateau`)),
+  gray(190)(line(`https://github.com/joeflateau`)),
+  gray(190)(line(`https://linkedin.com/in/joeflateau`)),
 ];
 
-console.info('\n' + message.join('\n') + '\n');
+console.info("\n" + message.join("\n") + "\n");
